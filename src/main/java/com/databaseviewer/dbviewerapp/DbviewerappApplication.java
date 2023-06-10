169dbe7af1083976b6558c259d8853db7d68c2d3
SzymonB5 <235831@edu.p.lodz.pl>
2023-06-10 17:22:47 +0200

    Fix

--- src/main/java/com/databaseviewer/dbviewerapp/DbviewerappApplication.java
+++ src/main/java/com/databaseviewer/dbviewerapp/DbviewerappApplication.java
@@ -6,18 +6,51 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashSet;
+
 @SpringBootApplication
 @RestController
-@RequestMapping("/test")
+@RequestMapping("")
 public class DbviewerappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DbviewerappApplication.class, args);
+//		System.out.println(DatabaseConnector.getDatabaseName());
+		 SpringApplication.run(DbviewerappApplication.class, args);
 	}
 
-	@GetMapping
+	@GetMapping("/test")
+	public String test1() {
+		return "123";
+	}
+
+	@GetMapping("/works")
+	public String test2() {
+		return "TESTTEST";
+	}
+
+	@GetMapping("/main")
 	public String test() {
-		return "Dupa";
+		StringBuilder stringBuilder = new StringBuilder("<h4>Currently using " + DatabaseConnector.getDatabaseName() + " database.</h4>\n");
+		HashSet<String> hashSet = DatabaseConnector.getDatabaseInfo();
+
+		stringBuilder.append("<form id=\"myForm\">");
+		stringBuilder.append("<select id=\"tableNameSelect\">");
+		hashSet.forEach(option -> stringBuilder.append("<option>").append(option).append("</option>"));
+		stringBuilder.append("</select>");
+		stringBuilder.append("</form>");
+
+		stringBuilder.append("<button type=\"button\" onclick=\"gotoTable()\">Pokaż zawartość tabeli</button>");
+
+		// stringBuilder.append("<div id=\"displayDiv\"></div>");
+
+		stringBuilder.append("<script>");
+		stringBuilder.append("function gotoTable() {");
+		stringBuilder.append("	var tableName = document.getElementById('tableNameSelect').value;");
+		stringBuilder.append("	window.location.href = '/tables/' + tableName;");
+		stringBuilder.append("}");
+		stringBuilder.append("</script>");
+
+		return stringBuilder.toString();
 	}
 
 }
