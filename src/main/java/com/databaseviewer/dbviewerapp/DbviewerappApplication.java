9e572e2be6c32cf83284af41d80935990075028f
SzymonB5 <235831@edu.p.lodz.pl>
2023-06-10 16:46:55 +0200

    Init commit

---
+++ src/main/java/com/databaseviewer/dbviewerapp/DbviewerappApplication.java
@@ -0,0 +1,23 @@
+package com.databaseviewer.dbviewerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/test")
+public class DbviewerappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbviewerappApplication.class, args);
+	}
+
+	@GetMapping
+	public String test() {
+		return "Dupa";
+	}
+
+}
---
+++ src/test/java/com/databaseviewer/dbviewerapp/DbviewerappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.databaseviewer.dbviewerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbviewerappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
